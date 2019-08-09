b1ba8886c5bed21ff782d017d0eb371b28d20362
jimpjoseph <jim.p.joseph@gmail.com>
2019-08-08 21:51:19 -0500

    completed chapeter 7

--- chp7/CriminalIntent/app/src/main/java/org/learning/criminalintent/CrimeFragment.java
+++ chp7/CriminalIntent/app/src/main/java/org/learning/criminalintent/CrimeFragment.java
@@ -6,6 +6,9 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
@@ -14,6 +17,8 @@ public class CrimeFragment extends Fragment {
 
     private Crime mCrime;
     private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,6 +48,18 @@ public class CrimeFragment extends Fragment {
             }
         });
 
+        mDateButton = v.findViewById(R.id.crime_date);
+        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setEnabled(false);
+
+
+        mSolvedCheckBox = v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                mCrime.setSolved(isChecked);
+            }
+        });
         return v;
     }
 
