ac1618bf757439040162091c766c0d1b6e863d2f
jimpjoseph <jim.p.joseph@gmail.com>
2019-08-14 19:42:44 -0500

    Created C1

---
+++ chp8/CI8C/app/src/main/java/org/learning/criminalintent/CrimeListActivity.java
@@ -0,0 +1,10 @@
+package org.learning.criminalintent;
+
+import androidx.fragment.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+    @Override
+    public Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
