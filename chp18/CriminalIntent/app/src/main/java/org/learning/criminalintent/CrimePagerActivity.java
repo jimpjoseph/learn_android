40f06ad384d8bd58c98e8a6f23ce612d892344ac
jimpjoseph <jim.p.joseph@gmail.com>
2019-09-10 17:08:39 -0500

    Started 18

---
+++ chp18/CriminalIntent/app/src/main/java/org/learning/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,66 @@
+package org.learning.criminalintent;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.List;
+import java.util.UUID;
+
+public class CrimePagerActivity extends AppCompatActivity implements CrimeFragment.Callbacks{
+
+
+    private static final String EXTRA_CRIME_ID = "org.learning.criminalintent.crime_id";
+
+    public static Intent newIntent(Context packageContent, UUID crimeId) {
+        Intent intent = new Intent(packageContent,CrimePagerActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+        return intent;
+    }
+
+    private ViewPager mViewPager;
+    private List<Crime> mCrimes;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actvitiy_crime_pager);
+
+        UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
+        mViewPager = findViewById(R.id.crime_view_pager);
+        mCrimes = CrimeLab.get(this).getCrimes();
+
+        FragmentManager fm = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fm) {
+            @Override
+            public Fragment getItem(int position) {
+                Crime crime = mCrimes.get(position);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
+
+        for (int i = 0; i < mCrimes.size(); i++) {
+            if (mCrimes.get(i).getId().equals(crimeId)) {
+                mViewPager.setCurrentItem(i);
+                break;
+            }
+        }
+    }
+
+
+    @Override
+    public void onCrimeUpdated(Crime crime) {
+        // Do nothing.
+    }
+}
