0b1b186e6ed8fc86c8f52ed80f2296ca22568b03
jimpjoseph <jim.p.joseph@gmail.com>
2019-09-12 21:22:32 -0500

    Completed chp 20

--- chp20/BeatBox/app/src/main/java/org/learning/beatbox/BeatBoxFragment.java
+++ chp20/BeatBox/app/src/main/java/org/learning/beatbox/BeatBoxFragment.java
@@ -14,9 +14,20 @@ import androidx.recyclerview.widget.RecyclerView;
 import org.learning.beatbox.databinding.FragmentBeatBoxBinding;
 import org.learning.beatbox.databinding.ListItemSoundBinding;
 
+import java.util.List;
+
+
 public class BeatBoxFragment extends Fragment {
+    private BeatBox mBeatBox;
+
     public static BeatBoxFragment newInstance() {
         return new BeatBoxFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mBeatBox = new BeatBox(getActivity());
     }
 
     @Override
@@ -25,7 +36,7 @@ public class BeatBoxFragment extends Fragment {
                 R.layout.fragment_beat_box, container, false );
 
         binding.recyclerView.setLayoutManager(new GridLayoutManager(getActivity(),3));
-        binding.recyclerView.setAdapter(new SoundAdapter());
+        binding.recyclerView.setAdapter(new SoundAdapter(mBeatBox.getSounds()));
         return binding.getRoot();
     }
 
@@ -34,15 +45,27 @@ public class BeatBoxFragment extends Fragment {
     private class SoundHolder extends RecyclerView.ViewHolder {
         private ListItemSoundBinding mBinding;
 
+
         private SoundHolder(ListItemSoundBinding binding) {
             super(binding.getRoot());
             mBinding = binding;
+            mBinding.setViewModel(new SoundViewModel(mBeatBox));
         }
 
+        public void bind(Sound sound) {
+            mBinding.getViewModel().setSound(sound);
+            mBinding.executePendingBindings();
+        }
 
     }
 
     private class SoundAdapter extends RecyclerView.Adapter<SoundHolder> {
+
+        private List<Sound> mSounds;
+
+        public SoundAdapter(List<Sound> sounds) {
+            mSounds = sounds;
+        }
 
         @NonNull
         @Override
@@ -56,12 +79,13 @@ public class BeatBoxFragment extends Fragment {
 
         @Override
         public void onBindViewHolder(@NonNull SoundHolder holder, int position) {
-
+            Sound sound = mSounds.get(position);
+            holder.bind(sound);
         }
 
         @Override
         public int getItemCount() {
-            return 0;
+            return mSounds.size();
         }
     }
 
---
+++ chp20/BeatBox/app/src/main/java/org/learning/beatbox/Sound.java
@@ -0,0 +1,21 @@
+package org.learning.beatbox;
+
+public class Sound {
+    private String mAssetPath;
+    private String mName;
+
+    public Sound(String assetPath) {
+        mAssetPath = assetPath;
+        String[] components = assetPath.split("/");
+        String fileName = components[components.length-1];
+        mName = fileName.replace(".wav", "");
+    }
+
+    public String getAssetPath() {
+        return mAssetPath;
+    }
+
+    public String getName() {
+        return mName;
+    }
+}
