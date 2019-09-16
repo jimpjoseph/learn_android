7d42627a172463cba2d6bad4fde30c99e6be624f
jimpjoseph <jim.p.joseph@gmail.com>
2019-09-16 07:57:31 -0500

    started chp 22

---
+++ chp22/BeatBox/app/src/test/java/org/learning/beatbox/SoundViewModelTest.java
@@ -0,0 +1,38 @@
+package org.learning.beatbox;
+
+import org.junit.Before;
+import org.junit.Test;
+
+//import static org.hamcrest.Matchers.is;
+//import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class SoundViewModelTest {
+
+    private BeatBox mBeatBox;
+    private Sound mSound;
+    private SoundViewModel mSubject;
+
+    @Before
+    public void setUp() throws Exception {
+        mBeatBox = mock(BeatBox.class);
+        mSound = new Sound("assetPath");
+        mSubject = new SoundViewModel(mBeatBox);
+        mSubject.setSound(mSound);
+    }
+
+    @Test
+    public void exponsesSoundNameAsTitle() {
+        assertThat(mSubject.getTitle(), is(mSound.getName()));
+    }
+
+    @Test
+    public void callsBeatBoxPlayOnButtonClicked() {
+        mSubject.onButtonClicked();
+        verify(mBeatBox).play(mSound);
+    }
+
+}
