b0b742dc92e3439146ee93af2ea150cb6ff0d19a
jimpjoseph <jim.p.joseph@gmail.com>
2019-09-27 18:44:55 -0500

    Completed Display

--- chp25/PhotoGallery/app/src/main/java/org/learning/photogallery/PhotoGallaryFragment.java
+++ chp25/PhotoGallery/app/src/main/java/org/learning/photogallery/PhotoGallaryFragment.java
@@ -6,17 +6,22 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PhotoGallaryFragment extends Fragment {
     public static final String TAG = PhotoGallaryFragment.class.getSimpleName();
 
     private RecyclerView mPhotoRecylerView;
+
+    private List<GalleryItem> mItems = new ArrayList<>();
 
     public static PhotoGallaryFragment newInstance() {
         return new PhotoGallaryFragment();
@@ -35,17 +40,70 @@ public class PhotoGallaryFragment extends Fragment {
         mPhotoRecylerView = v.findViewById(R.id.photo_recycler_view);
         mPhotoRecylerView.setLayoutManager(new GridLayoutManager(getActivity(),3));
 
+        setupAdapter();
         return v;
     }
 
+    private void setupAdapter() {
+        if (isAdded()) {
+            mPhotoRecylerView.setAdapter(new PhotoAdaper(mItems));
+        }
+    }
 
-    private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+
+    private class PhotoHolder extends RecyclerView.ViewHolder {
+        private TextView mTitleTextView;
+
+        public PhotoHolder(View itemView) {
+            super(itemView);
+            mTitleTextView = (TextView) itemView;
+        }
+
+        public void bindGaleryItem(GalleryItem item) {
+            mTitleTextView.setText(item.toString());
+        }
+    }
+
+
+    private class PhotoAdaper extends RecyclerView.Adapter<PhotoHolder> {
+
+        private List<GalleryItem> mGalleryItems;
+
+        public PhotoAdaper(List<GalleryItem> items) {
+            mGalleryItems = items;
+        }
 
         @Override
-        protected Void doInBackground(Void ... params) {
-            new FlickrFetchr().fetchItems();
-            return null;
+        public PhotoHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+            TextView textView = new TextView(getActivity());
+            return new PhotoHolder(textView);
         }
+
+        @Override
+        public void onBindViewHolder(PhotoHolder photoHolder, int position) {
+            GalleryItem item = mGalleryItems.get(position);
+            photoHolder.bindGaleryItem(item);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mGalleryItems.size();
+        }
+    }
+
+    private class FetchItemsTask extends AsyncTask<Void, Void, List<GalleryItem>> {
+
+        @Override
+        protected List<GalleryItem> doInBackground(Void ... params) {
+            return new FlickrFetchr().fetchItems();
+        }
+
+        @Override
+        protected void onPostExecute(List<GalleryItem> items) {
+            mItems = items;
+            setupAdapter();
+        }
+
     }
 
 }
