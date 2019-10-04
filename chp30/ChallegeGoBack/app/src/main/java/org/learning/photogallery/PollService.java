dde1b0f45a054e720fef779c43a626085eb15cfc
jimpjoseph <jim.p.joseph@gmail.com>
2019-10-04 13:04:43 -0500

    Starting challenge Go Back

---
+++ chp30/ChallegeGoBack/app/src/main/java/org/learning/photogallery/PollService.java
@@ -0,0 +1,129 @@
+package org.learning.photogallery;
+
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.IntentService;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.net.ConnectivityManager;
+import android.os.SystemClock;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class PollService extends IntentService {
+    private static final String TAG = "PollService";
+
+    //Set interval to 1 minute.
+    private static final long POLL_INTERVAL_MS = TimeUnit.MINUTES.toMillis(1);
+
+    public static final String ACTION_SHOW_NOTICATION = "org.learning.photogallery.SHOW_NOTIFCATION";
+
+    public static final String PERM_PRIVATE = "org.learning.photogallery.PRIVATE";
+
+    public static final String REQUEST_CODE = "REQUEST_CODE";
+    public static final String NOTIFICATION = "NOTIFICATION";
+
+    public static Intent newIntent(Context context) {
+        return new Intent(context, PollService.class);
+    }
+
+    public static void setAlarmService(Context context, boolean isOn) {
+        Intent i = PollService.newIntent(context);
+        PendingIntent pi = PendingIntent.getService(context, 0, i, 0);
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+        if (isOn) {
+            alarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME,
+                    SystemClock.elapsedRealtime(), POLL_INTERVAL_MS, pi);
+        } else {
+            alarmManager.cancel(pi);
+            pi.cancel();
+        }
+
+        QueryPreferences.setAlarmOn(context,isOn);
+    }
+
+    public static boolean isServiceAlarmOn(Context context) {
+        Intent i = PollService.newIntent(context);
+        PendingIntent pi = PendingIntent.getService(context, 0, i, PendingIntent.FLAG_NO_CREATE);
+        return pi != null;
+    }
+
+    public PollService() {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        if (!isNetworkAvailableAndConnected()) {
+            return;
+        }
+        Log.i(TAG, "Received an Intent: " + intent);
+
+        String query = QueryPreferences.getStoredQuery(this);
+        String lastResultId = QueryPreferences.getLastResultId(this);
+
+        List<GalleryItem> items;
+
+        if (query == null) {
+            items = new FlickrFetchr().fetchRecentPhotos();
+        } else {
+            items = new FlickrFetchr().searchPhotos(query);
+        }
+
+        String resultId = items.get(0).getId();
+        if (resultId.equals(lastResultId)) {
+            Log.i(TAG, "Got an old result: " + resultId);
+        } else {
+            Log.i(TAG, "Got a new result: " + resultId);
+
+            Resources resources = getResources();
+
+            Intent i = PhotGalleryActivity.newIntent(this);
+
+            PendingIntent pi = PendingIntent.getService(this, 0, i, 0);
+
+            Notification notification = new NotificationCompat.Builder(this)
+                    .setTicker(resources.getString(R.string.new_pictures_title))
+                    .setSmallIcon(android.R.drawable.ic_menu_report_image)
+                    .setContentTitle(resources.getString(R.string.new_pictures_text))
+                    .setContentText(resources.getString(R.string.new_pictures_title))
+                    .setContentIntent(pi)
+                    .setAutoCancel(true)
+                    .build();
+
+            //NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+            //notificationManagerCompat.notify(0, notification);
+            //sendBroadcast(new Intent(ACTION_SHOW_NOTICATION), PERM_PRIVATE);
+            showBackgroundNotification(0, notification);
+        }
+
+        QueryPreferences.setLastResultId(this, resultId);
+    }
+
+    private boolean isNetworkAvailableAndConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        boolean isNetworkAvailbale = cm.getActiveNetworkInfo() != null;
+        boolean isNetworkConnected = isNetworkAvailbale && cm.getActiveNetworkInfo().isConnected();
+
+        return isNetworkConnected;
+    }
+
+    private void showBackgroundNotification(int requestCode, Notification notification) {
+        Intent i = new Intent(ACTION_SHOW_NOTICATION);
+        i.putExtra(REQUEST_CODE, requestCode);
+        i.putExtra(NOTIFICATION, notification);
+        sendOrderedBroadcast(i, PERM_PRIVATE, null, null,
+                Activity.RESULT_OK, null, null);
+    }
+}
