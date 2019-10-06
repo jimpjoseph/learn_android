e4285127dd3630319a120d42f1019810e9928e55
jimpjoseph <jim.p.joseph@gmail.com>
2019-10-06 09:13:02 -0500

    Started Extension functions 3.3

--- learn_kotlin/chp3/src/example/functions.kt
+++ learn_kotlin/chp3/src/example/functions.kt
@@ -1,7 +1,26 @@
 package example
 
+import java.lang.StringBuilder
+
 fun main(args : Array<String>) {
-   val set = hashSetOf(1,7,53)
+    s3_1()
+    s3_2()
+}
+
+fun s3_2() {
+    val list = listOf(1,2,3)
+    println(list)
+    println(joinToString(list))
+    println(joinToString(list,"| "))
+    println(joinToString(list,"; ", "(", ")"))
+    println(joinToString(list,prefix = "# ", postfix = "; "))
+    println("Kotlin".lastChar())
+}
+
+
+
+fun s3_1() {
+    val set = hashSetOf(1,7,53)
     println(set.javaClass)
 
     val list = arrayListOf(1,7,5)
