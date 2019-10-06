39f6069cfdfbadfcea2803c89defc1dae05a496b
jimpjoseph <jim.p.joseph@gmail.com>
2019-10-05 21:31:04 -0500

    Completed review of in

--- learn_kotlin/chp02/src/examples/hello.kt
+++ learn_kotlin/chp02/src/examples/hello.kt
@@ -2,6 +2,7 @@ package examples
 
 import geometry.shapes.Rectangle
 import geometry.shapes.createRandomRectange
+import java.util.*
 
 fun main(args: Array<String>) {
     println("Hello, World!")
@@ -31,8 +32,57 @@ fun main(args: Array<String>) {
     println(eval1(Sum(Sum(Num(1), Num(2)),Num(4))))
     println(eval(Sum(Sum(Num(1), Num(2)),Num(4))))
     println(evalWithLogging(Sum(Sum(Num(1), Num(2)),Num(4))))
+
+    for( i in 1..100) {
+        print(fizzBuzz(i))
+    }
+    println("")
+    for (i in 100 downTo 1 step 2) {
+        print(fizzBuzz(i))
+    }
+    println("")
+
+    mapExample()
+
+    println(isLetter('q'))
+    println(isNotDigit('4'))
+    println(recognize('8'))
+    println("Kotlin" in "Java".."Scala")
 }
 
 fun max(a: Int, b: Int): Int {
     return if (a > b) a else b
+}
+
+fun fizzBuzz(i: Int) = when {
+    i % 15 == 0 -> "FizzBuzz "
+    i % 3 == 0 -> "Fizz "
+    i % 5 == 0 -> "Buzz "
+    else -> "$i "
+}
+
+fun mapExample() {
+    val binaryHeap = TreeMap<Char, String>()
+    for (c in 'A'..'F') {
+        val binary = Integer.toBinaryString(c.toInt())
+        binaryHeap[c] = binary
+    }
+    for ((letter, binary) in binaryHeap) {
+        println("$letter = $binary")
+    }
+
+    val list = arrayListOf("10", "11", "1001")
+    for ((index, element) in list.withIndex()) {
+        println("$index = $element")
+    }
+}
+
+fun isLetter(c : Char) = c in 'a'..'z' || c in 'A'..'Z'
+
+fun isNotDigit(c : Char) = c !in '0'..'9'
+
+fun recognize(c: Char) = when(c) {
+    in '0'..'9' -> "it's a digit"
+    in 'a'..'z', in 'A'..'Z' -> "It's a letter"
+    else -> "I don't know"
 }
