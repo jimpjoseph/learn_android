57d71da837837a8e15100bd377b18193af698b83
jimpjoseph <jim.p.joseph@gmail.com>
2019-10-07 22:05:51 -0500

    completed 4.3.1

--- learn_kotlin/chp04/src/examples/main.kt
+++ learn_kotlin/chp04/src/examples/main.kt
@@ -3,6 +3,7 @@ package examples
 fun main(args: Array<String>){
     println("Chapter 04")
     s4_1()
+    s4_3()
 }
 
 fun s4_1() {
@@ -16,4 +17,24 @@ fun s4_1() {
     but.setFocus(false)
 
     println(eval(Expr.Sum(Expr.Num(1), Expr.Sum(Expr.Num(2),Expr.Num(3)))))
+}
+
+class Client(val name:String, val postalCode : Int) {
+    override fun hashCode(): Int = name.hashCode() * 31 + postalCode.hashCode()
+    override fun toString(): String = "Client(name=$name, postalCode=$postalCode)"
+    override fun equals(other: Any?): Boolean {
+        if (other == null || other !is Client)
+            return false
+        return name == other.name && postalCode == other.postalCode
+    }
+}
+
+fun s4_3() {
+    val client1 = Client("James Joseph", 60606)
+    println(client1)
+    val client2 = Client("James Joseph", 60606)
+    println(client1 == client2)
+
+    val processed = hashSetOf(client1)
+    println(processed.contains(client2))
 }
