23ddc0da858ea86ffb163b22c87ca34645afd253
jimpjoseph <jim.p.joseph@gmail.com>
2019-10-08 07:59:02 -0500

    Completed setup of the UI.

--- chp33/Locatr/app/src/main/java/org/learning/locatr/LocatorFragment.java
+++ chp33/Locatr/app/src/main/java/org/learning/locatr/LocatorFragment.java
@@ -2,19 +2,38 @@ package org.learning.locatr;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
 
 public class LocatorFragment extends Fragment {
+
+    private ImageView mImageView;
+
     public static LocatorFragment newInstance() {
         return new LocatorFragment();
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstancestate) {
         View v = inflater.inflate(R.layout.fragment_locatr, container, false);
+
+        mImageView = v.findViewById(R.id.image);
         return v;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu,inflater);
+        inflater.inflate(R.menu.fragment_locatr, menu);
     }
 }
