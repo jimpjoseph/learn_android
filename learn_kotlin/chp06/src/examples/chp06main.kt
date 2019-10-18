afd70f82520c35c0596d2015258b59747df3ad46
James Joseph <jim.p.joseph@gmail.com>
2019-10-17 22:30:04 -0500

    6.1.4

--- learn_kotlin/chp06/src/examples/chp06main.kt
+++ learn_kotlin/chp06/src/examples/chp06main.kt
@@ -2,10 +2,32 @@ package examples
 
 fun main(args: Array<String>) {
     s6_1_1()
+    s6_1_3()
 }
 
 fun strLen(s: String?) = if (s != null) s.length else 0
 fun s6_1_1() {
     println(strLen("James"))
     println(strLen(null))
+}
+
+fun printAllCaps(s: String?) {
+    val allCaps = s?.toUpperCase()
+    println(allCaps)
+}
+
+class Employee(val name:String, val manager: Employee?)
+fun managerName(employee: Employee) : String? = employee.manager?.name
+
+fun strLenSafe(s: String?) : Int = s?.length ?: 0
+
+fun s6_1_3() {
+    printAllCaps("jim")
+    printAllCaps(null)
+    val ceo = Employee("Da Boss", null)
+    val dev = Employee("Bob Smith", ceo)
+    println(managerName(dev))
+    println(managerName(ceo))
+    println(strLenSafe("jim"))
+    println(strLenSafe(null))
 }
