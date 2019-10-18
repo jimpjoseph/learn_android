21d72cbd1037abd7b58c09db17bbf59fc74cd17e
James Joseph <jim.p.joseph@gmail.com>
2019-10-17 22:13:34 -0500

    Completed 6.1.1

---
+++ learn_kotlin/chp06/src/examples/chp06main.kt
@@ -0,0 +1,11 @@
+package examples
+
+fun main(args: Array<String>) {
+    s6_1_1()
+}
+
+fun strLen(s: String?) = if (s != null) s.length else 0
+fun s6_1_1() {
+    println(strLen("James"))
+    println(strLen(null))
+}
