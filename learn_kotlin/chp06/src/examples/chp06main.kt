f359c45af786f854cfc4b6e60e2e8e282e1294ee
jimpjoseph <jim.p.joseph@gmail.com>
2019-10-18 06:41:40 -0500

    Completed 6.1.5

--- learn_kotlin/chp06/src/examples/chp06main.kt
+++ learn_kotlin/chp06/src/examples/chp06main.kt
@@ -3,6 +3,7 @@ package examples
 fun main(args: Array<String>) {
     s6_1_1()
     s6_1_3()
+    s6_1_4()
 }
 
 fun strLen(s: String?) = if (s != null) s.length else 0
@@ -30,4 +31,23 @@ fun s6_1_3() {
     println(managerName(ceo))
     println(strLenSafe("jim"))
     println(strLenSafe(null))
+}
+
+class Person(val firstName: String, val lastName: String) {
+    override fun equals(other: Any?): Boolean {
+        val otherPerson = other as? Person ?: return false
+        return otherPerson.firstName == firstName &&
+                otherPerson.lastName == lastName
+    }
+
+    override fun hashCode(): Int {
+        return firstName.hashCode() *31 + lastName.hashCode()
+    }
+}
+
+fun s6_1_4() {
+    val p1  = Person("James", "Joseph")
+    val p2 = Person("James", "Joseph")
+    println(p1.equals(p2))
+    println(p1.equals("James Joseph"))
 }
