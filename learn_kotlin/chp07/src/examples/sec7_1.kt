f66600df13ce37138c59162f9ca6bede391e54f3
James Joseph <jim.p.joseph@gmail.com>
2019-10-20 20:38:43 -0500

    7.3.2

--- learn_kotlin/chp07/src/examples/sec7_1.kt
+++ learn_kotlin/chp07/src/examples/sec7_1.kt
@@ -92,6 +92,14 @@ operator fun MutablePoint.set(index: Int, value : Int) {
         else -> throw IndexOutOfBoundsException("Invalid coordinate $index")
     }
 }
+
+data class Rectangle(val upperLeft: Point, val lowerRight: Point)
+
+operator fun Rectangle.contains(p: Point): Boolean {
+    return p.x in upperLeft.x until lowerRight.x &&
+            p.y in upperLeft.y until lowerRight.y
+}
+
 fun s7_3() {
     println("**** 7.3.1 *************")
     val p = Point(10,20)
@@ -100,4 +108,8 @@ fun s7_3() {
     val pm = MutablePoint(10,20)
     pm[1] = 30
     println(pm)
+
+    val rect = Rectangle(Point(10,20),Point(50,50))
+    println(Point(20,30 ) in rect)
+    println(Point(5,5) in rect)
 }
