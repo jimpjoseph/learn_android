1f88f89ebe8b99e9d71cfd48c3ee23633e3f59c9
jimpjoseph <jim.p.joseph@gmail.com>
2019-10-22 17:08:23 -0500

    8.1.2

--- learn_kotlin/chp08/src/example/sec8_1.kt
+++ learn_kotlin/chp08/src/example/sec8_1.kt
@@ -1,8 +1,11 @@
 package example
 
+import java.lang.StringBuilder
+
 fun s8_1() {
     println("***  8.1 ***")
     s8_1_1()
+    s8_1_2()
 }
 
 fun s8_1_1() {
@@ -16,4 +19,28 @@ fun s8_1_1() {
     println(canReturnNull(4,5))
     funOrNull = sum
     println(funOrNull(4,5))
+}
+
+fun twoAndThreeOperation(operation: (Int, Int) -> Int) {
+    val result = operation(2,3)
+    println("The result is ${result}")
+}
+
+fun String.filter(predicate: (Char) -> Boolean): String {
+    val sb = StringBuilder()
+    for (index in 0 until length) {
+        val element = get(index)
+        if (predicate(element)) {
+            sb.append(element)
+        }
+    }
+    return sb.toString()
+}
+
+fun s8_1_2() {
+    println("*** 8.1.2 ***")
+    twoAndThreeOperation({a,b -> a + b})
+    twoAndThreeOperation({a,b -> a * b})
+    twoAndThreeOperation({a,b -> a - b})
+    println("a1b2#c3d".filter { it in 'a'..'z' })
 }
