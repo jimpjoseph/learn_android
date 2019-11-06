18886ecd3f77568fc876e9526ceecea3b707504b
jimpjoseph <jim.p.joseph@gmail.com>
2019-11-05 20:43:49 -0600

    9.1.3

--- learn_kotlin/chp09/src/example/s91.kt
+++ learn_kotlin/chp09/src/example/s91.kt
@@ -3,6 +3,7 @@ package example
 fun s9_1() {
     println("      9.1       ")
     s9_1_1()
+    s9_1_3()
 }
 
 
@@ -15,4 +16,26 @@ fun s9_1_1() {
     println(letters.slice<Char>(0..2))
     println(letters.slice(10..13))
     println(listOf(1,2,3,4).penultimate)
+}
+
+fun <T : Number> onHalf(value: T) : Double {
+    return value.toDouble() / 2
+}
+
+fun <T: Comparable<T>> max(first: T, second: T): T {
+    return if (first > second) first else second
+}
+
+fun <T> ensureTrailingPeriod(seq: T)
+    where T: CharSequence, T: Appendable {
+    if (!seq.endsWith('.')) {
+        seq.append('.')
+    }
+}
+fun s9_1_3() {
+    println(onHalf(3))
+    println(max("Java", "Kotlin"))
+    val helloWorld = StringBuilder("Hello, World")
+    ensureTrailingPeriod(helloWorld)
+    println(helloWorld)
 }
